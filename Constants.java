b9ae100e0ee0c32ed5a54dab9da3b2259dc60dae
yin02A <107928786+yin02A@users.noreply.github.com>
2022-06-21 20:29:54 +0800

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,18 @@
+package constants;
+
+public class Constants {
+
+    public static String PDD_clientId="fe189ac144124ff19865789f65c240a2";
+    //fe189ac144124ff19865789f65c240a2
+    //2b26fb50f49f42dca61aaf539cfc4243  //mine
+
+    public static String PDD_clientSecret="1a8ad10009fafeee0c92317466a1ba5854189860";
+    //1a8ad10009fafeee0c92317466a1ba5854189860
+    //845c5bad555ade9d08f9833c3a5b198334a0c030  //mine
+
+    public static String NEWS_AppKey="bfb78f3e279f240ccd44b5ddb9948ea0";
+    //bfb78f3e279f240ccd44b5ddb9948ea0
+    //2e28441af02131d9d5ca81494c2fc0df //mine
+
+    public static String JOKE_key="62fdcf3cc950992c95d0d4631e1e12dd";
+}
